9d9ad9ca193601df6b60dc712ee93aef4c59d36c
Sarah B <smblair87@gmail.com>
2020-02-09 10:12:22 -0800

    Version 1.0.0

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -12,6 +13,9 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 //Get the default connection
